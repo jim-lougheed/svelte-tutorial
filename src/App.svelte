8d6322ead491c00617dff45f448a1958c94f3a4b
Jim Lougheed <jims.codes@gmail.com>
2022-12-10 18:46:58 -0500

    Complete 9: Conditional Rendering

--- src/App.svelte
+++ src/App.svelte
@@ -1,28 +1,47 @@
 <script>
-	const name = "Jim";
-	const lastName = "<b>Lougheed</b>";
-	const hack = `<a href="#" onclick="alert('You have been hacked!')">Win a prize!</a>`;
-	const id = "heading";
-	const disabled = false;
-	const status = "success";
+	// VIDS 1-8
+	// const name = "Jim";
+	// const lastName = "<b>Lougheed</b>";
+	// const hack = `<a href="#" onclick="alert('You have been hacked!')">Win a prize!</a>`;
+	// const id = "heading";
+	// const disabled = false;
+	// const status = "success";
 	// const isPromoted = true;
-	const promoted = true;
+	// const promoted = true;
+
+	// VIDS 9-
+	const num = 5;
 </script>
 
 <main>
-	<h1>Hello, {name}!</h1>
+	<!-- VIDS 1-8 -->
+	<!-- <h1>Hello, {name}!</h1>
 	<div>{@html lastName}</div>
 	<div>{@html hack}</div>
 	<h2 class="underline" {id}>This is a heading</h2>
-	<h2 class={status}>Status</h2>
+	<h2 class={status}>Status</h2> -->
+
 	<!-- <h2 class={isPromoted ? "promoted" : ""}>Movie Title</h2> -->
 	<!-- <h2 class:promoted={isPromoted}>Movie Title</h2> -->
-	<h2 class:promoted>Movie Title</h2>
-	<button {disabled}>Bind</button>
+	<!-- <h2 class:promoted>Movie Title</h2>
+
+	<button {disabled}>Bind</button> -->
+
+	<!-- VIDS 9- -->
+	{#if num === 0}
+	<h2>The number is 0</h2>
+	{:else if num < 0}
+	<h2>The number is negative</h2>
+	{:else if num > 0}
+	<h2>The number is positive</h2>
+	{:else}
+	<h2>The number is not a number</h2>
+	{/if}
 </main>
 
 <style>
-	.underline {
+	/* VIDS 1-8 */
+	/* .underline {
 		text-decoration: underline;
 	}
 	.danger {
@@ -33,7 +52,7 @@
 	}
 	.promoted {
 		font-style: italic;
-	}
+	} */
 	main {
 		text-align: center;
 		padding: 1em;
