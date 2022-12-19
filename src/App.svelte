0c1a0811639f8a96312a9f474afe2523b9985ec3
Jim Lougheed <jims.codes@gmail.com>
2022-12-18 21:47:29 -0500

    Complete 18: Component Props

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,19 @@
 <script>
     import Greet from "./components/Greet.svelte";
+	const userName = "Jim";
+	const job = "developer";
+	const obj = {
+		name: "Barry",
+		heroName: "The Flash",
+	}
 </script>
 
 <main>
-	<Greet></Greet>
+	<Greet name="Bruce" heroName="Batman"></Greet>
+	<Greet name="Clark" heroName="Superman"></Greet>
+	<Greet name="Diana" heroName="Wonder Woman"></Greet>
+	<Greet name={userName} heroName={job}></Greet>
+	<Greet {...obj}></Greet>
 </main>
 
 <style>
--- src/components/Greet.svelte
+++ src/components/Greet.svelte
@@ -1,1 +1,6 @@
-<h2>Hello Jim</h2>
+<script>
+    export let name;
+    export let heroName = "Default";
+</script>
+
+<h2>Hello {name}, also known as {heroName}</h2>
