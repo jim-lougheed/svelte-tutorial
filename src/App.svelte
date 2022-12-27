11c9f176c9b09ac6db527cf659b4abddbe45839c
Jim Lougheed <jims.codes@gmail.com>
2022-12-27 14:53:05 -0500

    Complete 29: Dynamic Components

--- src/App.svelte
+++ src/App.svelte
@@ -3,7 +3,14 @@
     //import PostList from "./components/PostList.svelte";
 
 	// VID 28
-	import AutoFocus from "./components/AutoFocus.svelte";
+	// import AutoFocus from "./components/AutoFocus.svelte";
+
+	// VID 29
+	import TabA from "./components/TabA.svelte";
+	import TabB from "./components/TabB.svelte";
+	import TabC from "./components/TabC.svelte";
+
+	let activeTab = TabA;
 </script>
 
 <main>
@@ -11,7 +18,21 @@
 	<!-- <PostList></PostList> -->
 
 	<!-- VID 28 -->
-	<AutoFocus></AutoFocus>
+	<!-- <AutoFocus></AutoFocus> -->
+
+	<!-- VID 29 -->
+	<button on:click={() => activeTab = TabA}>TabA</button>
+	<button on:click={() => activeTab = TabB}>TabB</button>
+	<button on:click={() => activeTab = TabC}>TabC</button>
+
+	<!-- {#if (activeTab === 'TabA')}
+		<TabA></TabA>
+	{:else if (activeTab === 'TabB')}
+		<TabB></TabB>
+	{:else if (activeTab === 'TabC')}
+		<TabC></TabC>
+	{/if} -->
+	<svelte:component this={activeTab}></svelte:component>
 </main>
 
 <style>
---
+++ src/components/TabC.svelte
@@ -0,0 +1,1 @@
+<h3>TabC Content</h3>
