ffc35bc96ffdc67308fb9cfaf4b375c4bf444c7b
Jim Lougheed <jims.codes@gmail.com>
2022-12-27 15:40:33 -0500

    Complete 34: Custom Stores

--- src/components/Display.svelte
+++ src/components/Display.svelte
@@ -1,5 +1,5 @@
 <script>
-    import { count } from '../store/stores';
+    import { count, customCount } from '../store/stores';
     // import { onDestroy } from 'svelte';
 
     // let counter;
@@ -13,3 +13,10 @@
 
 <!-- <h2>Current count is {counter}</h2> -->
 <h2>Current count is {$count}</h2>
+<h2>Custom count is {$customCount}</h2>
+
+<button on:click={() => customCount.increment()}>Increment</button>
+<button on:click={() => customCount.increment(5)}>Increment 5</button>
+<button on:click={() => customCount.decrement()}>Decrement</button>
+<button on:click={() => customCount.decrement(5)}>Decrement 5</button>
+<button on:click={() => customCount.reset()}>Reset</button>
